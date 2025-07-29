ac3b3d2baaeb91e59807756535b32b8bd7427329
Jatin-sharma01 <amobsidian001@gmail.com>
2025-07-29 10:23:33 +0530

    UI Done

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -3,7 +3,31 @@ import React from 'react'
 const Colors = () => {
   return (
     <div>
-      colors
+      <h2 className="sidebar-title color-title">Colors</h2>
+      <label className='sidebar-label-container '>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>All
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Black
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>$Blue
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Red
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Green
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>White
+        </label>
     </div>
   )
 }
